6918fd625f290aa30fc790b1489006eaa14da8c2
ShaquanTurner <92876529+ShaquanTurner@users.noreply.github.com>
2022-03-29 12:46:59 -0500

    Add project files.

---
+++ VotingSystemv1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace VotingSystemv1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(301, 177);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 109);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.UseWaitCursor = true;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(519, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Community";
+            this.comboBox1.UseWaitCursor = true;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.AutoCompleteCustomSource.AddRange(new string[] {
+            "Kingston",
+            "Westmoreland"});
+            this.comboBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Kingston",
+            "Westmoreland",
+            "St. Elizabeth Parish"});
+            this.comboBox2.Location = new System.Drawing.Point(61, 69);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.Text = "Parishes ";
+            this.comboBox2.UseWaitCursor = true;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Vote";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(79, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select your Parish";
+            this.label1.UseWaitCursor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(550, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Select your Community";
+            this.label2.UseWaitCursor = true;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(328, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Voting System";
+            this.label3.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(328, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Select your Issue";
+            this.label4.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "VotingSystem";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ VotingSystemv1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VotingSystemv1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // checkedListBox1.Items.Add("testing ");
+        
+           
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //comboBox1.Items.Clear();
+            if (comboBox2.SelectedItem.ToString() == "Kingston")
+            {
+                // comboBox2.Items.Clear();
+                comboBox1.Items.Clear();
+                checkedListBox1.Items.Clear();
+                comboBox1.Items.Remove("Savanna-La-Mar ");
+                comboBox1.Items.Add("Red Hills");
+                comboBox1.Items.Add("Old hope road ");
+                checkedListBox1.Items.Add("Water pipe brust ");
+                checkedListBox1.Items.Add("noise pollution");
+                checkedListBox1.Items.Add("Scamming ");
+                checkedListBox1.Items.Add("Corruption  ");
+                checkedListBox1.Items.Add("Break-ins   ");
+                this.checkedListBox1.Items.Add("stealing");
+
+
+            }
+            
+            else if (comboBox2.SelectedItem.ToString() == "Westmoreland")
+            {
+                comboBox1.Items.Clear();
+                checkedListBox1.Items.Clear();
+                 checkedListBox1.Items.Remove("Water pipe brust ");
+                comboBox1.Items.Remove("Red Hills");
+                comboBox1.Items.Remove("Old hope road ");
+                comboBox1.Items.Add("Savanna-La-Mar");
+                checkedListBox1.Items.Add("Lack of Electricity ");
+
+            }
+                else
+            {
+                comboBox1.Items.Clear();
+                checkedListBox1.Items.Clear();
+                checkedListBox1.Items.Remove("Water pipe brust ");
+                comboBox1.Items.Remove("Red Hills");
+                comboBox1.Items.Remove("Old hope road ");
+                comboBox1.Items.Remove("Savanna-La-Mar");
+                comboBox1.Items.Add("Santa Cruz");
+                checkedListBox1.Items.Add("Lack of water ");
+                checkedListBox1.Items.Add("Crime ");
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+
+            //comboBox1.Items.Clear();
+            checkedListBox1.Items.Clear();
+            if (comboBox1.SelectedItem.ToString() == "Red Hills")
+            {
+
+                this.checkedListBox1.Items.Add("Water Pipe Brust  ");
+            }
+            else
+            {
+                this.checkedListBox1.Items.Add("stealing");
+                checkedListBox1.Items.Add("Noise Pollution");
+
+                //this.checkedListBox1.Items.Add("Lack of Electricity ");
+
+            }
+
+            
+
+            //comboBox1.Items.Clear();
+            /*checkedListBox1.Items.Clear();
+            if (comboBox1.SelectedItem.ToString() == "Red Hills")
+            {
+
+                this.checkedListBox1.Items.Add("Water Supply problem ");
+            }
+
+
+            else if (comboBox1.SelectedItem.ToString() == "Old hope road")
+            {
+
+                this.checkedListBox1.Items.Add("noise pollution");
+            }
+            else
+            {
+
+
+                this.checkedListBox1.Items.Add("Lack of Electricity ");//this.checkedListBox1.Items.Add("Lack of Electricity ");
+                
+            }
+
+            */
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
